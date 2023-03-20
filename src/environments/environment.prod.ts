de4cfd19eb69d1fdd0bd1fce33afdc154904335b
KaranRajgorVision <125949542+KaranRajgorVision@users.noreply.github.com>
2023-03-20 15:37:48 +0530

    removed keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseAPIURL : "https://99b5-59-144-124-36.in.ngrok.io/api/Student/",
-  Google_Map_Secret_API_KEY:"AIzaSyDt9bQQQvus68qbhkael9TS66qV-hjA7VA"
+  Google_Map_Secret_API_KEY:""
 };
